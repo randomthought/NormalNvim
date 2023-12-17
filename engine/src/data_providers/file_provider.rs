877fb6dcf46262675117726748929e61f5042ea6
Apple <randomthought@Malcolms-MacBook-Pro.local>
2023-12-16 20:50:36 -0500

    we can by a little more lazy ;)

--- engine/src/data_providers/file_provider.rs
+++ engine/src/data_providers/file_provider.rs
@@ -1,6 +1,6 @@
 use std::{
     fs::File,
-    io::{BufRead, BufReader, Lines},
+    io::{BufRead, BufReader},
     path::Path,
     pin::Pin,
 };
@@ -8,27 +8,12 @@ use std::{
 use anyhow::Result;
 use futures_util::{stream, Stream};
 
-struct FileStream {
-    reader: Lines<BufReader<File>>,
-}
-
-impl Iterator for FileStream {
-    type Item = Result<String>;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        self.reader
-            .next()
-            .map(|line| line.map_err(anyhow::Error::from))
-    }
-}
-
 pub fn create_stream(path: &Path) -> Result<Pin<Box<dyn Stream<Item = Result<String>>>>> {
     // TODO: You can use chain for multiple files https://doc.rust-lang.org/std/iter/trait.Iterator.html#method.chain
     let file = File::options().write(false).read(true).open(path)?;
     let reader = BufReader::new(file);
-    let fs = FileStream {
-        reader: reader.lines(),
-    };
+
+    let fs = reader.lines().map(|line| line.map_err(anyhow::Error::from));
 
     let stream = stream::iter(fs);
     let pin = Box::pin(stream);
