ebb90b8ad96bbc1f49f0d0b2146bc84a2cdf7e54
Apple <randomthought@Malcolms-MBP.lan>
2024-03-29 17:26:06 -0500

    reove dead code

--- domain/src/broker/broker.rs
+++ domain/src/broker/broker.rs
@@ -1,11 +1,12 @@
 use crate::{
+    broker::orders::pending::PendingKey,
     data::QouteProvider,
     models::{
         orders::{
             common::Side, filled_order::FilledOrder, market::Market,
             security_position::SecurityPosition,
         },
-        price::{common::Price, quote::Quote},
+        price::{candle::Candle, common::Price, quote::Quote},
         security::Security,
     },
     strategy::algorithm::StrategyId,
@@ -38,90 +39,36 @@ impl Broker {
             qoute_provider,
         }
     }
-}
 
-// #[async_trait]
-// impl EventHandler for Broker {
-//     async fn handle(&self, event: &Event) -> Result<(), crate::error::Error> {
-//         let Event::Market(event::model::Market::DataEvent(d)) = event else {
-//             return Ok(());
-//         };
-//
-//         let Some(candle) = d.history.last() else {
-//             return Ok(());
-//         };
-//
-//         let security = &d.security;
-//         let pending = self.orders.get_pending_order(security).await;
-//
-//         for p in pending {
-//             match p.order {
-//                 NewOrder::Limit(o) => {
-//                     let met = match o.order_details.side {
-//                         Side::Long => o.price >= candle.close,
-//                         Side::Short => o.price <= candle.close,
-//                     };
-//                     if !met {
-//                         continue;
-//                     }
-//
-//                     // TODO: with this implementation, you would not get the exact limit price
-//                     let m = Market::new(
-//                         o.order_details.quantity,
-//                         o.order_details.side,
-//                         o.security.to_owned(),
-//                         o.strategy_id(),
-//                     );
-//                     let order = NewOrder::Market(m);
-//                     self.place_order(&order).await?;
-//                 }
-//                 NewOrder::StopLimitMarket(o) => todo!(),
-//                 _ => continue,
-//             };
-//         }
-//
-//         todo!()
-//     }
-// }
+    pub async fn handle(&self, candle: &Candle) {
+        let pending_key = PendingKey::SecurityKey(candle.security.clone());
+        let pending_orders = self.orders.get_pending_order(pending_key).await;
 
-fn create_filled_order(
-    quantity: u64,
-    security: &Security,
-    side: Side,
-    quote: &Quote,
-    strategy_id: StrategyId,
-) -> Result<FilledOrder, crate::error::Error> {
-    let price = match side {
-        Side::Long => quote.ask,
-        Side::Short => quote.bid,
-    };
-
-    let order_id = Uuid::new_v4().to_string();
-
-    let datetime = SystemTime::now()
-        .duration_since(UNIX_EPOCH)
-        .map_err(|e| crate::error::Error::Any(e.into()))?;
-
-    let fo = FilledOrder::builder()
-        .with_order_id(order_id)
-        .with_date_time(datetime)
-        .with_price(price)
-        .with_security(security.to_owned())
-        .with_quantity(quantity)
-        .with_side(side)
-        .with_strategy_id(strategy_id)
-        .build()
-        .map_err(|e| crate::error::Error::Any(e.into()))?;
-
-    Ok(fo)
-}
-
-fn calculate_cost(security_position: &SecurityPosition, filled_order: &FilledOrder) -> Price {
-    let quantity = if security_position.side == filled_order.order_details.side {
-        -Decimal::from_u64(filled_order.order_details.quantity).unwrap()
-    } else {
-        Decimal::from_u64(filled_order.order_details.quantity).unwrap()
-    };
-
-    quantity * filled_order.price
+        for p in pending_orders.iter() {
+            // match p.order {
+            //     NewOrder::Limit(o) => {
+            //         let met = match o.order_details.side {
+            //             Side::Long => o.price >= candle.close,
+            //             Side::Short => o.price <= candle.close,
+            //         };
+            //         if !met {
+            //             continue;
+            //         }
+            //
+            //         // TODO: with this implementation, you would not get the exact limit price
+            //         let m = Market::new(
+            //             o.order_details.quantity,
+            //             o.order_details.side,
+            //             o.security.to_owned(),
+            //             o.strategy_id(),
+            //         );
+            //         let order = NewOrder::Market(m);
+            //         self.place_order(&order).await?;
+            //     }
+            //     NewOrder::StopLimitMarket(o) => todo!(),
+            //     _ => continue,
+            // };
+        }
+        todo!()
+    }
 }
--- domain/src/models/price/candle.rs
+++ domain/src/models/price/candle.rs
@@ -7,26 +7,18 @@ use crate::models::security::Security;
 use super::common::{Price, Resolution};
 
 #[derive(Builder, Debug, Clone, Eq, PartialEq)]
+#[builder(setter(prefix = "with"))]
 #[non_exhaustive]
 pub struct Candle {
-    #[builder(setter(prefix = "with"))]
     pub security: Security,
-    #[builder(setter(prefix = "with"))]
     pub resolution: Resolution,
-    #[builder(setter(prefix = "with"))]
     pub high: Price,
-    #[builder(setter(prefix = "with"))]
     pub open: Price,
-    #[builder(setter(prefix = "with"))]
     pub low: Price,
-    #[builder(setter(prefix = "with"))]
     pub close: Price,
-    #[builder(setter(prefix = "with"))]
     pub start_time: Duration,
-    #[builder(setter(prefix = "with"))]
     pub end_time: Duration,
     // The trading volume of the symbol in the given time period.
-    #[builder(setter(prefix = "with"))]
     pub volume: u64,
 }
 
