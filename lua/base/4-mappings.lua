b8b3f322fee072ceccf552833a4ad9f77f3fbc3a
randomthought <malcolm@randomthought.io>
2024-11-27 00:04:51 -0500

    added fat finger operations

--- lua/base/4-mappings.lua
+++ lua/base/4-mappings.lua
@@ -87,6 +87,13 @@ local icons = {
 }
 
 -- standard Operations -----------------------------------------------------
+-- fat fingers
+vim.api.nvim_create_user_command('WQ', 'wq', {})
+vim.api.nvim_create_user_command('Wq', 'wq', {})
+vim.api.nvim_create_user_command('W', 'w', {})
+vim.api.nvim_create_user_command('Qa', 'qa', {})
+vim.api.nvim_create_user_command('Q', 'q', {})
+
 maps.n["j"] =
 { "v:count == 0 ? 'gj' : 'j'", expr = true, desc = "Move cursor down" }
 maps.n["k"] =
